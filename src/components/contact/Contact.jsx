fe35ccd7d055725a435557b0a6f758da23d54078
Hardik Chauhan <hardikchauhan200305@gmail.com>
2025-08-17 21:18:05 +0530

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
   const emailValidation = () => {
     return String(email)
       .toLowerCase()
-      .match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/);
+      .match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
   };
 
   const handleSend = (e) => {
